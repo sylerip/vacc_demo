1e03dc19fed702f4f5a6ef0f7231717a993e307f
sylerip <sylerip@yahoo.com>
2022-01-07 17:54:16 +0800

    gen zpt proof qr code

---
+++ VaccinationDemo/objC/BulletProofsFunc.h
@@ -0,0 +1,23 @@
+//
+//  BulletProofsFunc.h
+//  VaccinationDemo
+//
+//  Created by Comp 631C on 7/1/2022.
+//  Copyright © 2022 guofeng. All rights reserved.
+//
+
+#ifndef BulletProofsFunc_h
+#define BulletProofsFunc_h
+
+
+#endif /* BulletProofsFunc_h */
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface BulletProofs:NSObject
+/* method declaration */
++ (NSString *)genDateProof:(NSString *)dateStr;
++ (BOOL)proofDate:(NSString *)proofStr;
+@end
